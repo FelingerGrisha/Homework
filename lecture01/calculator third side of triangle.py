4db43b49e11343321f764f24d58af8fc42596221
FelingerGrisha <91113664+FelingerGrisha@users.noreply.github.com>
2021-10-21 20:12:46 +0700

    Add files via upload

--- lecture01/calculator third side of triangle.py
+++ lecture01/calculator third side of triangle.py
@@ -1,1 +1,13 @@
+import math
 
+print("Введите длину 1 стороны треугольника: ")
+side1 = float(input())
+print("Введите длину 2 стороны треугольника: ")
+side2 = float(input())
+print("Введите величину угла между этими сторонами: ")
+angle = math.radians(float(input()))
+
+side3 = math.sqrt(side1 ** 2 + side2 ** 2 - 2 * side1 * side2 * math.cos(angle))
+
+print("Длина 3 стороны треугольника равна:", side3)
+input()
