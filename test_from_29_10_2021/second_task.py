f4c0c5c10e7af398ad3690dc9e5c71e5919ce9de
FelingerGrisha <91113664+FelingerGrisha@users.noreply.github.com>
2021-10-30 17:22:07 +0700

    Add files via upload

---
+++ test_from_29_10_2021/second_task.py
@@ -0,0 +1,41 @@
+programm = open("input.txt", "r").read().strip()
+programm = programm.split(",")
+
+for i in range(len(programm)):
+    if(programm[i].isdigit()):
+        programm[i] = int(programm[i])
+    else:
+        print("Во входной файле была найдена ошибка, в файле должны содержаться только целые числа, разделённые запятой")
+        exit(1)
+
+
+i = 0
+number1 = 0
+number2 = 0
+previous_programm = programm
+
+while programm[0] != 5290681:
+    for number1 in range(len(programm)):
+        for number2 in range(len(programm)):
+
+            programm = previous_programm
+            programm[1] = number1
+            programm[2] = number2
+
+            while i < len(programm):
+                if (programm[i] == 1):
+                    programm[programm[i + 3]] = programm[programm[i + 1]] + programm[programm[i + 2]]
+                    i += 4
+
+                if (programm[i] == 2):
+                    programm[programm[i + 3]] = programm[programm[i + 1]] * programm[programm[i + 2]]
+                    i += 4
+
+                if (programm == 99):
+                    break
+
+                i += 1
+
+
+
+print(number1, number2)
