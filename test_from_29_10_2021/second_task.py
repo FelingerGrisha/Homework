5b3065328fb210ebb43b74289d4b4a9ee2f2525a
FelingerGrisha <91113664+FelingerGrisha@users.noreply.github.com>
2021-10-30 20:12:33 +0700

    Update second_task.py

--- test_from_29_10_2021/second_task.py
+++ test_from_29_10_2021/second_task.py
@@ -14,27 +14,32 @@ number1 = 0
 number2 = 0
 previous_programm = programm
 
-while programm[0] != 5290681:
-    for number1 in range(len(programm)):
-        for number2 in range(len(programm)):
+for number1 in range(len(programm)):
+    for number2 in range(len(programm)):
 
-            programm = previous_programm
-            programm[1] = number1
-            programm[2] = number2
+        programm = previous_programm
+        programm[1] = number1
+        programm[2] = number2
 
-            while i < len(programm):
-                if (programm[i] == 1):
-                    programm[programm[i + 3]] = programm[programm[i + 1]] + programm[programm[i + 2]]
-                    i += 4
+        while i < len(programm):
 
-                if (programm[i] == 2):
-                    programm[programm[i + 3]] = programm[programm[i + 1]] * programm[programm[i + 2]]
-                    i += 4
+            if (programm[i] == 1):
 
-                if (programm == 99):
-                    break
+                programm[programm[i + 3]] = programm[programm[i + 1]] + programm[programm[i + 2]]
+                i += 4
 
-                i += 1
+            if (programm[i] == 2):
+
+                programm[programm[i + 3]] = programm[programm[i + 1]] * programm[programm[i + 2]]
+                i += 4
+
+            if (programm == 99):
+                break
+
+            i+=1
+
+        if(programm[0] == 5290681):
+            break
 
 
 
