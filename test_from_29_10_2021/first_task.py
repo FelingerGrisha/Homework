67b9036f469a1892cbb82c82a5911dd91d80ca39
FelingerGrisha <91113664+FelingerGrisha@users.noreply.github.com>
2021-10-31 00:21:01 +0700

    Update first_task.py

--- test_from_29_10_2021/first_task.py
+++ test_from_29_10_2021/first_task.py
@@ -3,35 +3,42 @@ programm = file.read().strip()
 programm = programm.split(",")
 
 for i in range(len(programm)):
-    if(programm[i].isdigit() and int(programm[i]) < len(programm)):
+    if (programm[i].isdigit() and int(programm[i]) < len(programm)):
         programm[i] = int(programm[i])
     else:
-        print("Во входном файле была найдена ошибка, в файле должны содержаться только целые числа от 0 до числа общего количества оп-кодов и их аргументов, разделённые запятой")
+        print(
+            "Во входном файле была найдена ошибка, в файле должны содержаться только целые числа от 0 до числа общего количества оп-кодов и их аргументов, разделённые запятой")
         exit(1)
 
-i = 0
+last_position_1 = programm[::-1].index(1)
+last_position_2 = programm[::-1].index(2)
 
+if(last_position_1 < 3 or last_position_2 < 3):
+     print("Во входном файле была найдена ошибка, не достаточно аргументов для оп-кода")
+     exit(1)
+
+i = 0
 programm[1] = 12
 programm[2] = 2
 
 while i < len(programm):
 
-    if(programm[i] == 1):
-        
-        programm[programm[i+3]] = programm[programm[i+1]] + programm[programm[i+2]]
-        i+= 4
+    if (programm[i] == 1):
+
+        programm[programm[i + 3]] = programm[programm[i + 1]] + programm[programm[i + 2]]
+        i += 4
         continue
 
-    if(programm[i] == 2):
-        
-        programm[programm[i+3]] = programm[programm[i+1]] * programm[programm[i+2]]
-        i+= 4
+    if (programm[i] == 2):
+
+        programm[programm[i + 3]] = programm[programm[i + 1]] + programm[programm[i + 2]]
+        i += 4
         continue
 
-    if(programm == 99):
+    if (programm == 99):
         break
 
-    i+= 1
+    i += 1
 
 file.close()
 
