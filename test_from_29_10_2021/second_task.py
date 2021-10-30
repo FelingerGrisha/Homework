d4449fc8fe66d8de9d3ec2e8f65a802578ad7121
FelingerGrisha <91113664+FelingerGrisha@users.noreply.github.com>
2021-10-30 21:36:06 +0700

    Update second_task.py

--- test_from_29_10_2021/second_task.py
+++ test_from_29_10_2021/second_task.py
@@ -2,7 +2,7 @@ programm = open("input.txt", "r").read().strip()
 programm = programm.split(",")
 
 for i in range(len(programm)):
-    if(programm[i].isdigit()):
+    if(programm[i].isdigit() and int(programm[i]) < len(programm)):
         programm[i] = int(programm[i])
     else:
         print("Во входном файле была найдена ошибка, в файле должны содержаться только целые числа от 0, разделённые запятой")
@@ -27,11 +27,13 @@ for number1 in range(len(programm)):
 
                 programm[programm[i + 3]] = programm[programm[i + 1]] + programm[programm[i + 2]]
                 i += 4
+                continue
 
             if (programm[i] == 2):
 
                 programm[programm[i + 3]] = programm[programm[i + 1]] * programm[programm[i + 2]]
                 i += 4
+                continue
 
             if (programm == 99):
                 break
